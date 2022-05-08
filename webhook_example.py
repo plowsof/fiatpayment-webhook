05b9dc0fcb97a06605fb9b112f167c86fc14f98a
plowsof <77655812+plowsof@users.noreply.github.com>
2022-05-08 20:30:56 +0100

    Update webhook_example.py

--- webhook_example.py
+++ webhook_example.py
@@ -82,3 +82,97 @@ async def get_body(request: Request,status_code=200):
         db_date_time = rows[0][8]
         wishlist_usd_notify(db_usd,db_ref_id,db_email,db_fname,db_lname,db_zip,db_street,db_cc,db_order_id,db_date_time)
     con.commit()
+
+    
+@app.post("/flask/fiat_donate")
+def square_checkout(fiatDonate: FiatDonate):
+    global LOCATION_ID
+    item_dict = fiatDonate.dict()
+    #pprint.pprint(item_dict)
+    '''
+    send_this = {
+    "fname": data[0].value,
+    "lname": data[1].value,
+    "street": data[2].value,
+    "zip": data[3].value,
+    "email": data[4].value,
+    "id": id,
+    "usd": usd
+    }
+    '''
+    wish_id = item_dict["id"]
+    usd_amount = item_dict["usd"]
+    email = item_dict["email"]
+    fname = item_dict["fname"]
+    lname = item_dict["lname"]
+    _zip = item_dict["zip"]
+    street = item_dict["street"]
+    cc = "TBD"
+    order_id = "TBD"
+    comment_name="Anonymous"
+    comment="Test comment"
+    ref_id = str(uuid.uuid4())[0:27] + "@" + wish_id
+    con = sqlite3.connect('card_orders.db')
+    cur = con.cursor()
+    create_orders_table = """ CREATE TABLE IF NOT EXISTS orders (
+                                usd integer,
+                                ref_id text PRIMARY KEY,
+                                email text,
+                                fname text,
+                                lname text,
+                                zip text,
+                                street text,
+                                cc text,
+                                order_id text,
+                                date_time text,
+                                comment text,
+                                comment_name text,
+                                amount_expected default 0 not null
+                            ); """
+
+    cur.execute(create_orders_table)
+    con.commit()
+
+
+    result = client.checkout.create_checkout(
+    location_id = LOCATION_ID,
+    body = {
+    "idempotency_key": str(uuid.uuid4()),
+    "order": {
+      "order": {
+                    "location_id": LOCATION_ID,
+                    "reference_id": ref_id,
+                    "customer_id": str(uuid.uuid4()),
+                    "line_items": [
+                                      {
+                                        "uid": str(uuid.uuid4()),
+                                        "name": "MAGIC Donation",
+                                        "quantity": "1",
+                                        "base_price_money": {
+                                          "amount": int(usd_amount),
+                                          "currency": "USD"
+                                        }
+                                      }
+                                ],
+                    "state": "OPEN"
+            },
+      "idempotency_key": str(uuid.uuid4())
+    },
+    "pre_populate_buyer_email": email,
+    #"redirect_url" : 'https://getwishlisted.xyz/flask/checkout',
+    "merchant_support_email" : "support@magicgrants.org"
+    });
+
+    if result.is_success():
+        #pprint.pprint(result.body)
+        body = result.body
+        sql = ''' INSERT INTO orders (usd,ref_id,email,fname,lname,zip,street,cc,order_id,date_time,comment,comment_name)
+                    VALUES(?,?,?,?,?,?,?,?,?,?,?,?)'''
+        print(f"[DEBUG]: inserting ref_id as: {ref_id}")
+        cur.execute(sql, (0,ref_id,email,fname,lname,_zip,street,cc,order_id,str(datetime.now()),comment,comment_name))
+        con.commit()
+        return body["checkout"]["checkout_page_url"]
+    elif result.is_error():
+        print(result.errors)
+    return '', 200
+
